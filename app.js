f67b2333da080e675a66569358b097661aaba7b2
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2021-01-21 22:51:51 +0700

    adding AJAX for fetchin the API

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const quotes = document.getElementById("quote");
 const author = document.getElementById("author");
+const button = document.getElementById("button");
 
-const getQuotes = async () => {
-  const request = await fetch('https://favqs.com/api/qotd');
-  const data = await request.json();
-  return data;
+window.onload = getQuotes = () => {
+  const url = "https://favqs.com/api/qotd";
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      quotes.innerHTML =`"${data.quote.body}"`;
+      author.innerHTML = `- ${data.quote.author}`;
+    }).catch(e => {
+      alert("Failed to fetch data");
+    });
 }
-
-getQuotes().then(quote => {
-  quotes.innerText = `"${quote.quote.body}"`;
-  author.innerText = `- ${quote.quote.author}`;
-})
